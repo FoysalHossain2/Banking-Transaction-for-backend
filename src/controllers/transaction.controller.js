67629c12afc3feabcbdcc55b1d72760b3814b7ad
Md.Foysal Hossain <fh375170@gmail.com>
2026-09-19 21:39:45 +0600

    feat: implement transaction creation logic with pending status and session management

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -2,6 +2,7 @@ const transactionModel = require("../models/transaction.model")
 const ledgerModel = require("../models/ledger.model")
 const emailService = require("../services/email.service")
 const accountModel = require("../models/account.model")
+const { default: mongoose } = require("mongoose")
 
 
 /**
@@ -101,4 +102,23 @@ async function createTransaction(req, res) {
             message: `Insufficient balance. Current balance is ${balance}. Requested amount is ${amount}`
         })
     }
+
+    /**
+     * 5. Create transaction (PENDING)
+     */
+
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
+    const transaction = await transactionModel.create({
+        fromAccount,
+        toAccount,
+        amount,
+        idempotencyKey,
+        status: "PENDING"
+    }, {session})
+
+    const debitLedgerEntry = await ledgerModel.create({
+        
+    })
 }
